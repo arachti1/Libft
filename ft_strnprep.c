a905355a0320aa467392cf03dc4c83c596a35f6d
larachti <leo.arachtingi@gmail.com>
2019-09-17 23:51:51 +0200

    norm

--- ft_strnprep.c
+++ ft_strnprep.c
@@ -6,13 +6,14 @@
 /*   By: larachti <larachti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/13 19:56:27 by larachti          #+#    #+#             */
-/*   Updated: 2019/09/14 02:43:07 by larachti         ###   ########.fr       */
+/*   Updated: 2019/09/17 23:51:19 by larachti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strnprep(char **str, char c, size_t len) {
+size_t		ft_strnprep(char **str, char c, size_t len)
+{
 	char *prep;
 
 	prep = ft_strnew(len);
--- ft_strtoupper.c
+++ ft_strtoupper.c
@@ -6,17 +6,19 @@
 /*   By: larachti <larachti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 23:43:54 by larachti          #+#    #+#             */
-/*   Updated: 2019/08/30 23:45:06 by larachti         ###   ########.fr       */
+/*   Updated: 2019/09/17 23:51:15 by larachti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*ft_strtoupper(char *s) {
+char		*ft_strtoupper(char *s)
+{
 	size_t		index;
 
 	index = 0;
-	while (s[index]) {
+	while (s[index])
+	{
 		s[index] = ft_toupper(s[index]);
 		index++;
 	}
